781e0ca4376737582a4d762b05f865712ea04cc0
korayzorlu <korayzorllu@gmail.com>
2023-01-24 16:11:05 +0300

    edit global

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
+@login_required(login_url = "/admin")
 def index(request):
 
     return render(request, "index/index.html")
