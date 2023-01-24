e41d04d0ea2eaa0c7c697e86ed5c37fc5e6c9018
korayzorlu <korayzorllu@gmail.com>
2023-01-24 14:26:45 +0300

    first commit

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+
+# Create your views here.
+
+
+def index(request):
+
+    return render(request, "index/index.html")
