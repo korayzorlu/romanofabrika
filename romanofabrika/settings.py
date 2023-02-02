ad0c0c14eb045810b5fb09ffe35234b04214f3a0
korayzorlu <korayzorllu@gmail.com>
2023-02-02 18:00:55 +0300

    edit global

--- romanofabrika/settings.py
+++ romanofabrika/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 
 from decouple import config
 
+import psycopg2
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,14 +80,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'romanofabrika.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'romanofabrika',
+        'USER' : 'romano',
+        'PASSWORD' : '1201341',
+        'HOST' : 'localhost',
+        'PORT' : '',
     }
 }
 
