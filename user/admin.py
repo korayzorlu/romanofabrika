f5a323814679b4c721cecb466383a9251926c46b
korayzorlu <korayzorllu@gmail.com>
2023-02-07 15:47:34 +0300

    edit admin.py

--- user/admin.py
+++ user/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Employee, UserCategory, StaffCategory
+from .models import Employee
 
 # Register your models here.
 
@@ -14,7 +14,7 @@ class EmployeeUser(admin.ModelAdmin):
     class Meta:
         model = Employee
 
-   
+    
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
