911256647d24145bf480c4ef3516ff35a17d17a7
korayzorlu <korayzorllu@gmail.com>
2023-02-15 18:02:03 +0300

    edit global

--- romanofabrika/settings.py
+++ romanofabrika/settings.py
@@ -151,3 +151,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#USE_THOUSAND_SEPARATOR = True
