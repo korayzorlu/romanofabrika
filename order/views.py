6224d23a71b29dc3c2bb4a03e3024d38e787feba
korayzorlu <korayzorllu@gmail.com>
2023-03-01 17:57:09 +0300

    edit views.py

--- order/views.py
+++ order/views.py
@@ -49,7 +49,7 @@ def updateOrders(request):
                    "SiparisID" : -1,
                    "SiparisKaynagi" : "",
                    "SiparisKodu" : "",
-                   "SiparisTarihiBas" : datetime(2022, 1, 1),
+                   "SiparisTarihiBas" : datetime(2022, 8, 1),
                    #"SiparisTarihiSon" : datetime(2023, 2, 9),
                    "StrSiparisDurumu" : "",
                    "TedarikciID" : -1,
