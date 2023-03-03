ecade10c97cae5fb077b901fda19f3f0417567a3
korayzorlu <korayzorllu@gmail.com>
2023-03-03 14:27:38 +0300

    edit views.py

--- product/views.py
+++ product/views.py
@@ -53,7 +53,7 @@ def updateProducts(request):
                   "ToplamStokAdediBas" : 0,
                   "ToplamStokAdediSon" : 1000,
                   "TedarikciID" : 0}
-    urunSayfalama = {"BaslangicIndex" : 200,
+    urunSayfalama = {"BaslangicIndex" : 300,
                      "KayitSayisi" : 100,
                      "SiralamaDegeri" : "YayinTarihi",
                      "SiralamaYonu" : "desc"}
